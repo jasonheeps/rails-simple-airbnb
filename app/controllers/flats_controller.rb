883b32d422849134d2c159ce31d344ed5fa95c17
Philipp Schaefer <pschaefer@posteo.net>
2021-01-14 21:41:09 +0100

    added most basic functionality and design

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,23 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    Flat.create!(flat_params)
+    redirect_to flats_path
+  end
+
+  def show
+    @flat = Flat.find_by_id(params[:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
 end
